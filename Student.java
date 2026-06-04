ac0860b644a509324c00e6ab3f64d9129d93618c
V Shanjay <shanjayv23@gmail.com>
2026-06-04 12:36:17 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,9 +1,24 @@
-class Student
+import java.util.*;
+class student
 {
-String name = "RAXX";
+int id;
+String name;
+student(int id, String name)
+{
+this.id = id;
+this.name = name;
+}
+}
+class Main_Student
+{
 public static void main(String[] args)
 {
-Student s = new Student();
-System.out.println(s.name);
+ArrayList<student> students = new ArrayList<>();
+students.add(new student(101,"Raxx"));
+students.add(new student(102,"Shaxx"));
+for(student s : students)
+{
+System.out.println(s.id+" "+s.name);
+}
 }
 }
